3757f606b04a9d0fb2234f3d011050ba9a1f957f
Alex Vakanski <avakanski@gmail.com>
2023-08-04 10:56:18 -0600

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -14,21 +14,22 @@ version = '0.1.0'
 # -- General configuration
 
 extensions = [
+    'nbsphinx',
     'sphinx.ext.duration',
     'sphinx.ext.doctest',
     'sphinx.ext.autodoc',
     'sphinx.ext.autosummary',
     'sphinx.ext.intersphinx',
     'sphinx_rtd_theme',
-    'nbsphinx',
+    'sphinx_gallery.load_style',
 ]
 
-intersphinx_mapping = {
-    'python': ('https://docs.python.org/3/', None),
-    'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
-}
+# intersphinx_mapping = {
+#    'python': ('https://docs.python.org/3/', None),
+#    'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
+# }
 
-intersphinx_disabled_domains = ['std']
+# intersphinx_disabled_domains = ['std']
 
 # Add any paths that contain templates here, relative to this directory.
 # The template links to a custom style css to increase the width of displayed content
@@ -45,6 +46,11 @@ html_static_path = ['_static']
 master_doc = 'index'
 
 highlight_language = 'python3'
+
+nbsphinx_execute_arguments = [
+    "--InlineBackend.figure_formats={'svg', 'pdf'}",
+    "--InlineBackend.rc={'figure.dpi': 96}",
+]
 
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
