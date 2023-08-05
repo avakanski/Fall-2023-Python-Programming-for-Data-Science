b0f24b0f74a066e0db1328806afee33dc0cab662
Alex Vakanski <avakanski@gmail.com>
2023-08-05 15:20:18 -0600

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -3,7 +3,6 @@
 import sphinx_rtd_theme
 
 # -- Project information
-
 project = 'Fall 2023 Python Programming for Data Science'
 copyright = '2023, Alex Vakanski'
 author = 'Alex Vakanski'
@@ -12,7 +11,6 @@ release = '0.1'
 version = '0.1.0'
 
 # -- General configuration
-
 extensions = [
     'nbsphinx',
     'sphinx.ext.duration',
@@ -24,17 +22,16 @@ extensions = [
     'sphinx_gallery.load_style',
 ]
 
-# intersphinx_mapping = {
-#    'python': ('https://docs.python.org/3/', None),
-#    'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
-# }
+intersphinx_mapping = {
+    'python': ('https://docs.python.org/3/', None),
+    'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
+}
 
 # Add any paths that contain templates here, relative to this directory.
 # The template links to a custom style css to increase the width of displayed content
 templates_path = ['_templates']
 
 # -- Options for HTML output
-
 # -- The theme to use for HTML and HTML Help pages.  See the documentation for a list of builtin themes.
 html_theme = 'sphinx_rtd_theme'
 
@@ -45,12 +42,5 @@ master_doc = 'index'
 
 highlight_language = 'python3'
 
-#nbsphinx_execute_arguments = [
-#    "--InlineBackend.figure_formats={'svg', 'pdf'}",
-#    "--InlineBackend.rc={'figure.dpi': 96}",
-#]
-
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
-
-
